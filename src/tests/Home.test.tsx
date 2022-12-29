523f7cfc87b02fd73478fd492331e412daf6dbca
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2022-12-29 09:02:33 -0300

    test: render home

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -3,7 +3,17 @@ import { render, screen } from '@testing-library/react';
 
 import Home from '../pages';
 
-test('home', () => {
-  render(<Home />);
-  expect(screen.getByText('Next Vitest Boilerplate')).toBeInTheDocument();
+describe('Home page', () => {
+  it('renders the page', () => {
+    render(<Home />);
+    const title = screen.getByText('Rayo Reader');
+    expect(title).toBeInTheDocument();
+    
+    const textbox = screen.getByRole('textbox', { name :'Enter text here' });
+    expect(textbox).toBeInTheDocument();
+
+    const button = screen.getByRole('button', { name: 'Read' });
+    expect(button).toBeInTheDocument();
+  });
 });
+
