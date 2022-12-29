95f0db99de48ae1926716498cff2511f8109db53
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2022-12-29 09:06:01 -0300

    test: enter text and read

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import Home from '../pages';
 
@@ -9,11 +10,23 @@ describe('Home page', () => {
     const title = screen.getByText('Rayo Reader');
     expect(title).toBeInTheDocument();
     
-    const textbox = screen.getByRole('textbox', { name :'Enter text here' });
+    const textbox = screen.getByRole('textbox', { name: 'Enter text here' });
     expect(textbox).toBeInTheDocument();
 
     const button = screen.getByRole('button', { name: 'Read' });
     expect(button).toBeInTheDocument();
+  });
+
+  it('can enter text into the textbox and read it', () => {
+    render(<Home />);
+    const textbox = screen.getByRole('textbox', { name: 'Enter text here' });
+    const button = screen.getByRole('button', { name: 'Read' });
+
+    userEvent.type(textbox, 'Hello World');
+    expect(textbox).toHaveValue('Hello World');
+
+    userEvent.click(button);
+    expect(textbox).not.toBeInTheDocument();
   });
 });
 
