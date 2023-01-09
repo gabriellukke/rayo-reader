dcb85f19615a38608bb2a00797919edb456d1967
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2023-01-09 16:09:06 -0300

    feat: add reader provider in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import ReaderProvider from '../context/Reader';
 
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   /* eslint-disable react/jsx-props-no-spreading */
-  return <Component {...pageProps} />;
+  return (
+    <ReaderProvider>
+      <Component {...pageProps} />
+    </ReaderProvider>
+  )
 }
+
