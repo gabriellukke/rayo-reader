a13a82e22681f864bff7c9b1bd53f398e11bfa76
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2023-01-20 14:52:16 -0300

    feat: add version at title

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className="w-full h-16 bg-elevation-1 flex items-center">
+    <div className="w-full h-16 bg-elevation-1 flex items-center justify-center">
       <h1 className="text-gray-300 text-3xl ml-5">Rayo Reader</h1>
+      <div className="flex items-center ml-5 text-gray-300">Version 0.1.0 (Beta)</div>
     </div>
   );
 }
